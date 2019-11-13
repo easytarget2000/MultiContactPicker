215e009d4206e5b777b968529dbacf44084f473f
Michel Sievers <michel@easy-target.eu>
2019-11-13 18:38:34 +0100

    Clean up whitespace

--- multicontactpicker/src/main/java/com/wafflecopter/multicontactpicker/MultiContactPickerActivity.java
+++ multicontactpicker/src/main/java/com/wafflecopter/multicontactpicker/MultiContactPickerActivity.java
@@ -3,18 +3,17 @@ package com.wafflecopter.multicontactpicker;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.graphics.drawable.DrawableCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.l4digital.fastscroll.FastScrollRecyclerView;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
@@ -177,9 +176,7 @@ public class MultiContactPickerActivity extends AppCompatActivity implements Mat
         }
 
     }
-
-
-
+    
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
